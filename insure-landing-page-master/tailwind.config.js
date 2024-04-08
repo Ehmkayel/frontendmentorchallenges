e580c9bba3f93777a1390d06f9afa4e682e03ccc
lamidimorufat <lamidimorufat0@gmail.com>
2024-04-08 22:38:02 +0100

    done configuring tailwind

--- insure-landing-page-master/tailwind.config.js
+++ insure-landing-page-master/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '420px',
+      md: '768px',
+      lg: '900px',
+      xl: '1440px'
+
+    },
+
+    extend: {
+      colors:{
+        darkViolet: 'hsl(256, 26%, 20%)',
+        grayishBlue: 'hsl(216, 30%, 68%)',
+        veryDarkViolet: 'hsl(270, 9%, 17%)',
+        darkGrayishViolet: 'hsl(273, 4%, 51%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      }
+    },
   },
   plugins: [],
 }
